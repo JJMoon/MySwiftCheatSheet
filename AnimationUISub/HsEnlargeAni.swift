a3553c03ce287ab26fcfa5d40495716d98f4f562
Jongwoo Moon <pluminmaster@gmail.com>
2016-08-28 13:38:41 +0900

    확대 뷰.
    Protocol Oriented Example

---
+++ AnimationUISub/HsEnlargeAni.swift
@@ -0,0 +1,84 @@
+//
+//  HsEnlargeAni.swift
+//  HS Monitor
+//
+//  Created by Jongwoo Moon on 2016. 7. 25..
+//  Copyright © 2016년 IMLab. All rights reserved.
+//
+
+import Foundation
+
+protocol someProtocol {
+
+    func sampleFUnc()
+}
+
+
+protocol PrtclEnlargeView {
+    var targetView: UIView? { get set }
+
+    var tarPoint: CGPoint? { get set }
+    var baseOrigin: CGPoint? { get set }
+    var scal : CGFloat { get set }
+
+    /// 임시로 생성하는 뷰, 버튼
+    var bgView: UIView? { get set } // 백그라운드의 반투명 버튼
+    var bttnClose: UIButton? { get set } // 화면 닫는 투명 버튼
+    var enlargeClsr: ((Void) -> Void)? { get set }
+
+    mutating func animation()
+}
+
+extension PrtclEnlargeView  where Self: UIViewController {
+    mutating func animation() {
+
+        //print("  there here")
+
+        if baseOrigin == nil {
+            print("  Enlarge ")
+            baseOrigin = targetView!.center
+            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 10, initialSpringVelocity: 3,
+                                       options: [   ],
+                                       animations: {
+                                        self.targetView!.center = self.tarPoint!
+                                        self.targetView!.transform = CGAffineTransformMakeScale(self.scal, self.scal)})
+            { [] (finished: Bool) in    }
+
+            bgView = UIView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: UIScreen.mainScreen().bounds.size))
+            bgView!.backgroundColor = colHalfTransGray
+
+            //  view.addSubview(bgView!)  백그라운드 뷰 없다....
+            self.view.bringSubviewToFront(targetView!)
+            //view.addSubview(bttnClose!)
+
+            if (enlargeClsr != nil) { enlargeClsr!() }
+
+        } else {
+            print("  Shrink  ")
+            UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 10, initialSpringVelocity: 3,
+                                       options: [   ],
+                                       animations: {
+                                        self.targetView!.transform = CGAffineTransformIdentity
+                                        self.targetView!.center = self.baseOrigin!
+                                        self.targetView!.setNeedsLayout()
+                                        self.targetView!.layoutIfNeeded()
+                })
+            { [] (finished: Bool) in
+                self.targetView!.setNeedsLayout()
+                self.targetView!.layoutIfNeeded()
+                self.baseOrigin = nil
+            }
+
+            bgView?.removeFromSuperview()
+            bttnClose?.removeFromSuperview()
+        }
+        
+        
+    }
+
+    mutating func shrinkAction(sender: UIButton!) {
+        animation()
+
+    }
+    
+}
